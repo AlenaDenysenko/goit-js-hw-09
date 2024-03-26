c18f6b12bd9a39aa29cb03094a3ba263e3c65a11
AlenaDenysenko <naopiya28@gmai.com>
2024-03-26 13:28:50 +0200

    error

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
   return {
